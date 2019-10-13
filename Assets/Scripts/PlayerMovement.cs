bca87f4e6a1fdd4c36d6b145814ef7a3b41c602e
Sebastian Maj <wolftech@cogeco.ca>
2019-10-13 00:50:34 -0400

    Character + Camera Movement
    - Added Character drag movement
    - Added Camera smooth following
    - Added a little trail effect for character to show speed in a cartoon way

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    private bool bStartDrag = false;
+    private Vector2 startMousePos;
+    private Vector2 ballForceDirection;
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown() {
+        bStartDrag = true;
+        startMousePos = Input.mousePosition;
+    }
+
+    private void Update() {
+        if(bStartDrag) {
+            Vector2 newMousePos = Input.mousePosition;
+            Vector2 mouseDifference = newMousePos - startMousePos;
+            ballForceDirection = -mouseDifference;
+        }
+    }
+
+    private void OnMouseUp() {
+        if(bStartDrag) {
+            bStartDrag = false;
+            rigidbody.AddForce(ballForceDirection);
+        }
+    }
+}
