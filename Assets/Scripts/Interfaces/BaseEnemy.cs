01c1de9baf6c68306587a1fd6fb706d23efbf34f
Sebastian Maj <sebastian@redpiston.com>
2019-12-05 01:41:49 -0500

    BaseEnemy

---
+++ Assets/Scripts/Interfaces/BaseEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour
+{
+    private float health = 100f;
+    public float maxHealth = 100f;
+
+    private Transform _target;
+
+    public virtual void Move() {
+
+    }
+
+    public virtual void Damage(float damage) {
+        health -= damage;
+        CheckDeath();
+    }
+
+    public virtual void CheckDeath() {
+
+    }
+}
